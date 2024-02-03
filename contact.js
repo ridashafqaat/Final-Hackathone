4a01d6455d7861ffa2a646755d3fe5d597d2d722
ridashafqaat <ridashafqat282@gmail.com>
2024-02-03 09:42:59 +0500

    Final-Hackathone

---
+++ contact.js
@@ -0,0 +1,166 @@
+//   navbar
+
+let items = document.querySelectorAll('.item');
+let action = document.getElementById('action');
+let icondowns = document.querySelectorAll('.fa-chevron-down');
+
+
+items.forEach(item => {
+    item.addEventListener('click', function (e) {
+        if (this.classList.contains('active') || e.target.classList.contains('fa-chevron-down')) {
+            return;
+        }
+        items.forEach(remove_active => {
+            remove_active.classList.remove('active');
+        });
+        console.log(e.target);
+        this.classList.add('active');
+        document.documentElement.style.setProperty('--height-begin', action.offsetHeight + 'px');
+        document.documentElement.style.setProperty('--top-begin', action.offsetTop + 'px');
+        document.documentElement.style.setProperty('--height-end', this.offsetHeight + 'px');
+        document.documentElement.style.setProperty('--top-end', this.offsetTop + 'px');
+        action.classList.remove('runanimation');
+        void action.offsetWidth;
+        action.classList.add('runanimation');
+    }, false)
+})
+icondowns.forEach(icon => {
+    icon.addEventListener('click', function () {
+        this.classList.toggle('showMenuChild');
+        items.forEach(item => {
+            if (item.classList.contains('active')) {
+                document.documentElement.style.setProperty('--height-end', item.offsetHeight + 'px');
+                document.documentElement.style.setProperty('--top-end', item.offsetTop + 'px');
+                return;
+            }
+        });
+    })
+})
+
+
+
+
+//    menue button
+let menu_icon_box = document.querySelector(".menu_icon_box");
+let imp = document.querySelector(".imp");
+
+menu_icon_box.onclick = function () {
+    menu_icon_box.classList.toggle("active");
+    imp.classList.toggle("active");
+}
+
+
+
+
+
+let usernameRef = document.getElementById("username");
+let passwordRef = document.getElementById("password");
+let eyeL = document.querySelector(".eyeball-l");
+let eyeR = document.querySelector(".eyeball-r");
+let handL = document.querySelector(".hand-l");
+let handR = document.querySelector(".hand-r");
+let normalEyeStyle = () => {
+    eyeL.style.cssText = `
+    left:0.6em;
+    top: 0.6em;
+  `;
+    eyeR.style.cssText = `
+  right:0.6em;
+  top:0.6em;
+  `;
+};
+let normalHandStyle = () => {
+    handL.style.cssText = `
+        height: 2.81em;
+        top:8.4em;
+        left:7.5em;
+        transform: rotate(0deg);
+    `;
+    handR.style.cssText = `
+        height: 2.81em;
+        top: 8.4em;
+        right: 7.5em;
+        transform: rotate(0deg)
+    `;
+};
+//When clicked on username input
+usernameRef.addEventListener("focus", () => {
+    eyeL.style.cssText = `
+    left: 0.75em;
+    top: 1.12em;  
+  `;
+    eyeR.style.cssText = `
+    right: 0.75em;
+    top: 1.12em;
+  `;
+    normalHandStyle();
+});
+//When clicked on password input
+passwordRef.addEventListener("focus", () => {
+    handL.style.cssText = `
+        height: 6.56em;
+        top: 3.87em;
+        left: 11.75em;
+        transform: rotate(-155deg);    
+    `;
+    handR.style.cssText = `
+    height: 6.56em;
+    top: 3.87em;
+    right: 11.75em;
+    transform: rotate(155deg);
+  `;
+    normalEyeStyle();
+});
+//When clicked outside username and password input
+document.addEventListener("click", (e) => {
+    let clickedElem = e.target;
+    if (clickedElem != usernameRef && clickedElem != passwordRef) {
+        normalEyeStyle();
+        normalHandStyle();
+    }
+});
+
+
+// form alert code
+function display() {
+
+    if (document.getElementById("username").value == "") {
+        alert("please enter your name");
+        return false;
+    } else if (document.getElementById("password").value == "") {
+        alert("please enter your password");
+        return false;
+    } else {
+        validation()
+    }
+}
+
+function validation() {
+    swal("Good job!", "You clicked the button!", "success")
+}
+
+
+
+// loader
+// var loader = document.getElementById("pre_loader");
+
+// window.addEventListener("load", function () {
+//     setTimeout(function () {
+//         loader.style.display = "none";
+//     }, 2000);
+// });
+
+window.addEventListener('scroll', function () {
+    var scroll = this.document.querySelector('.scrollTop');
+    scroll.classList.toggle("active", window.scrollY > 500)
+})
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
+
+
---
+++ practice.js
@@ -0,0 +1,221 @@
+//   navbar
+
+let items = document.querySelectorAll('.item');
+let action = document.getElementById('action');
+let icondowns = document.querySelectorAll('.fa-chevron-down');
+
+
+items.forEach(item => {
+    item.addEventListener('click', function (e) {
+        if (this.classList.contains('active') || e.target.classList.contains('fa-chevron-down')) {
+            return;
+        }
+        items.forEach(remove_active => {
+            remove_active.classList.remove('active');
+        });
+        console.log(e.target);
+        this.classList.add('active');
+        document.documentElement.style.setProperty('--height-begin', action.offsetHeight + 'px');
+        document.documentElement.style.setProperty('--top-begin', action.offsetTop + 'px');
+        document.documentElement.style.setProperty('--height-end', this.offsetHeight + 'px');
+        document.documentElement.style.setProperty('--top-end', this.offsetTop + 'px');
+        action.classList.remove('runanimation');
+        void action.offsetWidth;
+        action.classList.add('runanimation');
+    }, false)
+})
+icondowns.forEach(icon => {
+    icon.addEventListener('click', function () {
+        this.classList.toggle('showMenuChild');
+        items.forEach(item => {
+            if (item.classList.contains('active')) {
+                document.documentElement.style.setProperty('--height-end', item.offsetHeight + 'px');
+                document.documentElement.style.setProperty('--top-end', item.offsetTop + 'px');
+                return;
+            }
+        });
+    })
+})
+
+
+
+
+//    menue button
+let menu_icon_box = document.querySelector(".menu_icon_box");
+let imp = document.querySelector(".imp");
+
+menu_icon_box.onclick = function () {
+    menu_icon_box.classList.toggle("active");
+    imp.classList.toggle("active");
+}
+
+// document.onclick = function(e){
+//     if (!menu_icon_box.contains(e.target) && !main_menu.contains(e.target) ) {
+//         menu_icon_box.classList.remove("active");
+//         main_menu.classList.remove("active");
+//     }
+// }
+
+
+// carsoul 1
+
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function () {
+    showSlider('next');
+}
+
+prevDom.onclick = function () {
+    showSlider('prev');
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if (type === 'next') {
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    } else {
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
+
+// loader
+var loader = document.getElementById("pre_loader");
+
+window.addEventListener("load", function () {
+    setTimeout(function () {
+        loader.style.display = "none";
+    }, 2000);
+});
+
+
+
+function search_box() {
+    let filter = document.getElementById('mysearch').ariaValueMax.toUpperCase();
+
+    let item = document.querySelectorAll('.product');
+
+    let l = document.getElementsByTagName('h2');
+
+    for (var i = 0; i <= l.length; i++) {
+        let a = item[i].getElementsByTagName('h2')[0];
+
+        let value = a.innerHTML || a.innerText || a.textContent;
+
+        if (value.toUpperCase().indexOf(filter) > -1) {
+            item[i].style.display = "";
+        }
+        else {
+            item[i].style.display = "none";
+        }
+    }
+}
+
+// carsoul 2
+
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    // Handle scrollbar thumb drag
+    scrollbarThumb.addEventListener("mousedown", (e) => {
+        const startX = e.clientX;
+        const thumbPosition = scrollbarThumb.offsetLeft;
+        const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+
+        // Update thumb position on mouse move
+        const handleMouseMove = (e) => {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = thumbPosition + deltaX;
+            // Ensure the scrollbar thumb stays within bounds
+            const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+            scrollbarThumb.style.left = `${boundedPosition}px`;
+            imageList.scrollLeft = scrollPosition;
+        }
+        // Remove event listeners on mouse up
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        }
+        // Add event listeners for drag interaction
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    });
+    // Slide images according to the slide button clicks
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+    // Show or hide slide buttons based on scroll position
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+    }
+    // Update scrollbar thumb position based on image scroll
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    }
+    // Call these two functions when image list scrolls
+    imageList.addEventListener("scroll", () => {
+        updateScrollThumbPosition();
+        handleSlideButtons();
+    });
+}
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
+
+// scrolltop
+
+window.addEventListener('scroll', function(){
+    var scroll = this.document.querySelector('.scrollTop');
+    scroll.classList.toggle("active" , window.scrollY > 500)
+})
+
+function scrollToTop(){
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    })
+}
+
+
